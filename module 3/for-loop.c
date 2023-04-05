dc883ab30e4fa958e9cfabfb2d94853d73693c77
AKToran <aktoran60@gmail.com>
2023-04-06 03:30:31 +0600

    while.c

---
+++ module 3/for-loop.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    for(i=1,n=0;i<=10;i=i+1)
+    {
+        n=n+i;
+        printf("%d\n",n);
+    }
+    return 0;
+}
---
+++ module 3/while.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    while(i=1)
+    {
+        printf("hello\n");
+        i++;
+    }
+    
+    return 0;
+}
